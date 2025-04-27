da8ada11102d357fa510febe05551996178eaf84
“yeooonn” <rladuswjd0303@naver.com>
2025-04-27 22:33:11 +0900

    controllers 와 providers 의 역할 학습
    - AppController: 클라이언트 요청(url)을 받아 해당 함수를 실행
    - AppService: 실행할 비즈니스 로직(함수)을 정의하고 관리

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController], // 클라이언트 요청(url)을 받아 해당 함수를 실행험
+  providers: [AppService], // 실행할 비즈니스 로직(함수)을 정의하고 관리함
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
+// 비즈니스 로직 실행
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello Nest!';
+  }
+  getHi(): string {
+    return 'Hi Nest!';
   }
 }

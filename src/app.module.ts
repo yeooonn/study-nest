9be39cd06b4456bf35ddabbe525fa1e474b870ea
“yeooonn” <rladuswjd0303@naver.com>
2025-04-27 22:59:55 +0900

    movies 컨트롤러 추가 및 다양한 데코레이터 학습
    - @Get(), @Post() 등: HTTP 요청 메서드에 매핑
    - @Param(): 요청 파라미터 값을 가져옴

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController], // 클라이언트 요청(url)을 받아 해당 함수를 실행험
-  providers: [AppService], // 실행할 비즈니스 로직(함수)을 정의하고 관리함
+  controllers: [MoviesController], // 클라이언트 요청(url)을 받아 해당 함수를 실행험
+  providers: [], // 실행할 비즈니스 로직(함수)을 정의하고 관리함
 })
 export class AppModule {}
